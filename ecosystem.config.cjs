99ab2cbf16b4d32af24feab2467bbe2243a52e2b
Rodrigo <rsbertoa90@gmail.com>
2025-07-31 23:18:25 -0300

    adaptando ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'recrearsesuenoslucidos.com',
-      script: './current/server/index.mjs',
-      interpreter: 'node',
+      // Cambia el 'script' a 'npm'
+      script: 'npm',
+      // Pasa el comando 'start' como argumento
+      args: 'run start',
+      // Establece el directorio de trabajo a la ruta del proyecto
+      cwd: './current',
+      // Opcional, pero recomendado
       env: {
         NODE_ENV: 'production'
       }
